d3684fd0f3e7094c164dbe1cfe04dfc2d40f40ee
Austin Oblow <austinoblow97@gmail.com>
2023-10-18 17:33:56 -0500

    completed lab, all tests passing

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function P(props) {
+
+  return (
+    <>
+      {props.bio ? <p>{props.bio}</p> : null}
+    </>
+    )
+}
+
+
+
+function About(props) {
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <P bio={props.bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
+
+
 
 export default About;
